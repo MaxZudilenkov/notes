69a0a045e1cde241ed8b1cbd2e34c134733e2fea
Max <shalyapapro@yandex.ru>
2022-02-22 14:34:34 +0300

    created note model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    # Модель заметки
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='note', verbose_name='Пользователь')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время публикации')
