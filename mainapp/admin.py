41155a1493f5e51cbacaa6cde441c3191d67344c
Max <shalyapapro@yandex.ru>
2022-02-22 14:39:08 +0300

    registered model in admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from mainapp.models import Note
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'published_at')
+
+
+admin.site.register(Note, NoteAdmin)
